5736b4ba82d844be32790c194dca57dec4932539
Chaitanya Ravuri <20ChaituR@users.noreply.github.com>
2024-12-09 11:18:10 -0500

    Update flash attention install

--- environment_setup.sh
+++ environment_setup.sh
@@ -18,7 +18,7 @@ pip install --upgrade pip
 conda install -c nvidia cuda-toolkit -y
 
 # Install FlashAttention2
-pip install https://github.com/Dao-AILab/flash-attention/releases/download/v2.5.8/flash_attn-2.5.8+cu122torch2.3cxx11abiFALSE-cp310-cp310-linux_x86_64.whl
+pip install flash-attn --no-build-isolation
 
 # Install VILA
 pip install -e .
